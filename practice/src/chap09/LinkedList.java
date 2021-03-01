a60109aec8f8f764d8640d9640522e3aeb49af4c
cjg <coolcjg@naver.com>
2021-03-01 21:02:34 +0900

    링크드리스트

---
+++ practice/src/chap09/LinkedList.java
@@ -0,0 +1,147 @@
+package chap09;
+
+import java.util.Comparator;
+
+public class LinkedList<E> {
+	
+	//노드
+	class Node<E>{
+		private E data;
+		private Node<E> next;
+		
+		Node(E data, Node<E> next){
+			this.data = data;
+			this.next = next;
+		}
+	}
+	
+	private Node<E> head;
+	private Node<E> crnt;
+	
+	
+	public LinkedList() {
+		head = crnt = null;
+	}
+	
+	public E search(E obj, Comparator<? super E> c) {
+		Node<E> ptr = head;
+		
+		while(ptr != null) {
+			if(c.compare(obj, ptr.data) == 0) {
+				crnt = ptr;
+				return ptr.data;
+			}
+			ptr = ptr.next;
+		}
+		return null;
+		
+	}
+	
+	//머리에 노드 삽입
+	public void addFirst(E obj) {
+		Node<E> ptr = head;
+		head = crnt = new Node<E>(obj, ptr);
+	}
+	
+	//꼬리에 노드 삽입
+	public void addLast(E obj) {
+		if(head == null)
+			addFirst(obj);
+		else {
+			Node<E> ptr = head;
+			while(ptr.next != null)
+				ptr = ptr.next;
+			ptr.next = crnt = new Node<E>(obj, null);
+		}
+	}
+	
+	public void removeFirst() {
+		if(head != null) {
+			head = crnt = head.next;
+		}
+	}
+	
+	// 꼬리 노드를 삭제
+	public void removeLast() {
+		if(head != null) {
+			if(head.next == null) {
+				removeFirst();
+			}else {
+				Node<E> ptr = head;
+				Node<E> pre = head;
+				
+				while(ptr.next != null) {
+					pre = ptr;
+					ptr = ptr.next;
+				}
+				pre.next = null;
+				crnt = pre;
+			}
+		}
+	}
+	
+	public void remove(Node p) {
+		if(head != null) {
+			if(p == head)
+				removeFirst();
+			else {
+				Node<E> ptr = head;
+				
+				while(ptr.next != p) {
+					ptr = ptr.next;
+					if(ptr == null) return;
+				}
+				
+				ptr.next = p.next;
+				crnt = ptr;
+			}
+		}
+	}
+	
+	//선택 노드를 삭제
+	public void removeCurrentNode() {
+		remove(crnt);
+	}
+	
+	//모든 노드를 삭제
+	public void clear() {
+		while(head != null) {
+			removeFirst();
+		}
+		crnt = null;
+	}
+	
+	
+	//선택 노드를 하나 뒤쪽으로 이동
+	public boolean next() {
+		if(crnt == null || crnt.next == null) {
+			return false;
+		}
+		crnt = crnt.next;
+		return true;
+	}
+	
+	public void printCurrentNode() {
+		if(crnt == null)
+			System.out.println("선택한 노드가 없습니다.");
+		else
+			System.out.println(crnt.data);
+	}
+	
+	//모든 노드를 출력
+	public void dump() {
+		Node<E> ptr = head;
+		
+		while(ptr != null) {
+			System.out.println(ptr.data);
+			ptr = ptr.next;
+		}
+	}
+	
+	
+	
+	
+	
+	
+
+}
