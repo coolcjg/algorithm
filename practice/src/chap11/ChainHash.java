93ee0ff66224e54823f82f05371c7944f6ddb140
coolcjg <coolcjg@naver.com>
2021-04-18 10:32:19 +0900

    해시2

--- practice/src/chap11/ChainHash.java
+++ practice/src/chap11/ChainHash.java
@@ -27,8 +27,90 @@ public class ChainHash<K, V> {
 		
 	}
 	
-
+	
+	private int size;
+	private Node<K, V>[] table;
 	
 	
+	//생성자
+	public ChainHash(int capacity) {
+		try {
+			table = new Node[capacity];
+			this.size = capacity;
+		} catch(OutOfMemoryError e) {
+			this.size = 0;
+		}
+	}
+	
+	public int hashValue(Object key) {
+		return key.hashCode() % size;
+	}
+	
+	//키 값 key를 갖는 요소의 검색 (데이터를 반환)
+	public V search(K key) {
+		int hash = hashValue(key);
+		Node<K, V> p = table[hash];
+		
+		while(p!=null) {
+			if(p.getKey().equals(key))
+				return p.getValue();
+			p = p.next;
+		}
+		return null;
+	}
+	
+	
+	//키 값 key, 데이터 data를 갖는 요소의 추가
+	public int add(K key, V data) {
+		int hash = hashValue(key);
+		Node<K, V> p = table[hash];
+		
+		while(p!=null) {
+			if(p.getKey().equals(key))
+				return 1;
+			p = p.next;
+		}
+		
+		Node<K, V> temp = new Node<K, V>(key, data, table[hash]);
+		table[hash] = temp;
+		return 0;
+	}
+	
+	
+	//키 값 key를 갖는 요소의 삭제
+	public int remove(K key) {
+		int hash = hashValue(key);
+		Node<K, V> p = table[hash];
+		Node<K, V> pp = null;
+		
+		while( p != null) {
+			if(p.getKey().equals(key)) {
+				if(pp == null)
+					table[hash] = p.next;
+				else
+					pp.next = p.next;
+				
+				return 0;
+				
+			}
+			
+			pp = p;
+			p = p.next;
+		}
+		return 1;	
+	}
+	
+	// 해시 테이블을 덤프
+	public void dump() {
+		for(int i = 9; i< size; i++) {
+			Node<K, V> p = table[i];
+			System.out.printf("%02d ", i);
+			while(p!=null) {
+				System.out.printf("-> %s (%s) ", p.getKey(), p.getValue());
+				p = p.next;
+			}
+			System.out.println();
+		}
+	}
 
 }
